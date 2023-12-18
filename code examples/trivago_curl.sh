fed2abaa0f2606bc363fada5910112b34d3aed1c
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2023-12-18 13:08:13 +0200

    Adding code examples/trivago_curl.sh

---
+++ code examples/trivago_curl.sh
@@ -0,0 +1,4 @@
+curl --user user:pass \
+'https://realtime.oxylabs.io/v1/queries' \
+-H "Content-Type: application/json" \
+-d '{"source": "universal", "url": "https://www.trivago.com/en-us/odr/hotels-chicago-illinois?search=200-14411&_gl=1*1u91udd*_up*mq..&gclid=trivago"}'
